9ce108c6ba92b91d642432844a948f3e651db52f
debabin <dmitriybabinup@gmail.com>
2024-11-23 18:49:09 +0700

    main 🧊 add socket instance to server action

--- app/server/socket.ts
+++ app/server/socket.ts
@@ -1,6 +1,6 @@
 import type { Server as HttpServer } from "node:http";
 import type { Server } from "socket.io";
 
-export const NextWsHttpServer = Symbol.for('NextWs_HttpServer');
+export const NextWsHttpServer = Symbol.for('next_socket_io');
 export const setSocketInstance = (socket: Server) => Reflect.set(globalThis, NextWsHttpServer, socket);
 export const getSocketInstance = () => Reflect.get(globalThis, NextWsHttpServer) as HttpServer;
